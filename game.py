88e2cf0beb4c812d89a0c0d1c3139e4976abd024
Jacob Møller <jmj1999@gmail.com>
2018-10-02 09:41:06 +0200

    Added Ascii art & basic code

--- game.py
+++ game.py
@@ -1,3 +1,22 @@
-#Author: Jacob M
-#Author: Oliver Thejl Eriksen
-#hi
+#Project Name: "Python Hangman Game" 
+#Author: Jacob Møller & Oliver Thejl Eriksen
+#Updated: 2/10 2018
+
+import random
+
+dkWordList = ["Sporvogn", "Skole"]
+dkWordNumberChosen = random.randint(0, len(dkWordList)-1)
+dkWord = dkWordList[dkWordNumberChosen]
+print(dkWord)
+for i in range(len(dkWord)):
+    print("_", end=" ")
+def hangmanOne():
+    print("")
+    print("    ————--")
+    print("    |    |")
+    print("    |           ")
+    print("    |        ")
+    print("    |     ")
+    print("  / | \       ")
+    print("/   |   \ ")
+hangmanOne()
