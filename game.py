688b1b166b9a0fb52b728b7894b5904e65c47792
Jacob Møller <jmj1999@gmail.com>
2018-10-03 10:26:38 +0200

    Commented the game.py code

--- game.py
+++ game.py
@@ -5,11 +5,20 @@
 import random
 import hangmanascii
 
+#Wordlist to choose from
 dkWordList = ["Sporvogn", "Skole"]
+#Chooses a random word in the wordlist array
 dkWordNumberChosen = random.randint(0, len(dkWordList)-1)
+#Selects the text based on the number
 dkWord = dkWordList[dkWordNumberChosen]
-print(dkWord)
+#Prints underscores the number of letters
 for i in range(len(dkWord)):
     print("_", end=" ")
-
+    
+#Calling hangman animation after 7 errors (wrong letters)
 hangmanascii.hangman(7)
+
+
+
+#Prints the word that the user should guess TEMPORARY
+print(dkWord)
